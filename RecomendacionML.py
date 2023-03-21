824d5b53e4c616f0c60583350d6789867224c50b
ElmerDaza <elmerdazamindiola@gmail.com>
2023-03-21 13:02:55 -0500

    sistema de recomendacion

---
+++ RecomendacionML.py
@@ -0,0 +1,34 @@
+import pickle, numpy as np
+
+# cargar modelo
+with open('modelo_recomendacion.pkl', 'rb') as archivo:
+    modelo = pickle.load(archivo)
+
+# funcion para predecir
+def predecir(userid:int,movieid:str):
+    #arrego de userid
+    arreglo=np.array([userid])
+    #transformacion de movieid
+    if movieid[0]=='a':
+        movieid= '19'+movieid[2:]
+    elif movieid[0]=='d':
+        movieid= '29'+movieid[2:]
+    elif movieid[0]=='h':
+        movieid= '39'+movieid[2:]
+    elif movieid[0]=='n':
+        movieid= '49'+movieid[2:]
+    else:
+        return {'mensaje':'este movieid '+movieid+' no es correcto'}
+    #arreglo de movieid
+    movieid=int(movieid)
+    arr =np.array([movieid])
+    #combinacion de los arreglos
+    predic = np.stack((arreglo,arr),axis=1)
+    #prediccion
+    resultado=modelo.predict([predic])
+    if resultado[0][0]>3.1:
+        return True
+    else:
+        return False
+    
+predecir(1,'ns3205')
